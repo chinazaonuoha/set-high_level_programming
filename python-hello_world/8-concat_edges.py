e706e7334c84e7fcaa9fe1f326b0229734e0e349
chinazaonuoha <developerworkpace@gmail.com>
2026-07-12 15:35:59 +0100

    print object-oriented programming with Python, followed by a new line.

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = "object-oriented programming with Python"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:66] + str[106:112] + str[0:6])
