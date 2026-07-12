cab1f57f7b625755f2a9b5b701fa173307734219
chinazaonuoha <developerworkpace@gmail.com>
2026-07-12 16:34:06 +0100

    print object-oriented programming with Python, followed by a new line.

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import sys
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-sys.stdout.write(str[39:66] + str[106:112] + str[0:6] + "\n")
+print(str[39:66], str[106:112], str[0:6])
