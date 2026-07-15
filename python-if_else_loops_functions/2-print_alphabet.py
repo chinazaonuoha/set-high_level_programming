803f3b8830791ceee9ddcf346d0a815657d439d3
chinazaonuoha <developerworkpace@gmail.com>
2026-07-15 11:12:48 +0100

    Write a program that prints the ASCII alphabet, in lowercase,

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -2,7 +2,7 @@
 def print_alphabet():
     """Print the alphabet in lowercase."""
     for letter in range(97, 123):
-        print(chr(letter), end="")
+        print(f"{chr(letter)}", end="")
     print()
 
 
