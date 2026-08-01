2012846fc4013680e4b6d6560980f4b2656bd235
chinazaonuoha <developerworkpace@gmail.com>
2026-08-01 16:12:43 +0100

    Write a program that imports all functions from the file calculator_1.py and handles basic operations.

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -9,8 +9,6 @@ if __name__ == "__main__":
         '*': mul,
         '/': div
     }
-
-
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
