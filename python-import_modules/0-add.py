ff28e86ccc90ae97455d28f5a672b50c4ed1924b
chinazaonuoha <developerworkpace@gmail.com>
2026-08-01 03:13:18 +0100

    Write a program that imports the function def add(a, b)

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import add_0
-
 
 if __name__ == "__main__":
+    from add_0 import add
+
     a = 1
     b = 2
-    result = add_0.add(a, b)
+    result = add(a, b)
+
     print("{} + {} = {}".format(a, b, result))
