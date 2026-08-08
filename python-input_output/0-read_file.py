70041e6a7283355db93bcf56c158cc120e5e5e4d
chinazaonuoha <developerworkpace@gmail.com>
2026-08-08 20:12:57 +0100

    Pycodestyle validation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,6 +9,7 @@ You don't need to manage file permission or file doesn't exist exceptions.
 You are not allowed to import any module
 """
 
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
