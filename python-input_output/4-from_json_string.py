769d97f258e7820230d5125e0cfb732a7f92a7f4
chinazaonuoha <developerworkpace@gmail.com>
2026-08-08 21:59:43 +0100

    Write a function that writes an Object to a text file, using a JSON representation:

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -10,7 +10,6 @@ the JSON string doesn't represent an object.
 
 
 def from_json_string(my_str):
-    """Returns an object (Python data structure)
-    represented by a JSON string."""
+    """Returns an object (Python data structure)represented by a JSON string."""
     import json
     return json.loads(my_str)
