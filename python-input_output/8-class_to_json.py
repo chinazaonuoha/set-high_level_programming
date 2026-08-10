c7c6106b0decfccf10da1d24bb3744b5baecbc77
chinazaonuoha <developerworkpace@gmail.com>
2026-08-10 10:45:33 +0100

    Write a function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object:

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,3 +1,9 @@
+"""
+This module contains a function that converts a class object to a dictionary
+for JSON serialization.
+"""
+
+
 def class_to_json(obj):
     """Returns the dictionary description with
     a simple data structure
