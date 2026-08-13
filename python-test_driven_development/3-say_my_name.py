54f03e5fa313a96272c7ac17e7143398c3ccb1eb
chinazaonuoha <developerworkpace@gmail.com>
2026-08-13 13:23:51 +0100

    Write a function that prints My name is <first name> <last name>

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,11 +3,14 @@ Module that defines a function to print a full name.
 """
 
 
-def say_my_name(first_name, last_name=""):
+def say_my_name(first_name="", last_name=""):
     """Prints 'My name is <first_name> <last_name>'."""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print(f"My name is {first_name} {last_name}".rstrip())
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name} ")
