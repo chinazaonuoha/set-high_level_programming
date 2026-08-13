ad8c4d2529fd56dca80c266fe8e6e89ceeaf87f6
chinazaonuoha <developerworkpace@gmail.com>
2026-08-13 13:12:20 +0100

    """Prints 'My name is <first_name> <last_name>'."""

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,26 +1,17 @@
-#!/usr/bin/python3
 """
-This module contains a function that prints a formatted
-string with the given first and last name.
-The function takes two parameters: first_name and last_name.
-It raises a TypeError if either parameter is not a string.
-If last_name is not provided, it defaults to an empty string.
-If first_name is an empty string, it raises a ValueError.
-The function returns a string in the format
-"My name is <first_name> <last_name>".
+Module that defines a function to print a full name.
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints a formatted full name."""
-    if not isinstance(first_name, str) or (
-        last_name and not isinstance(last_name, str)
-    ):
-        raise TypeError("first_name and last_name must be strings")
-
+    """Prints 'My name is <first_name> <last_name>'."""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
     if not first_name:
         raise ValueError("first_name cannot be empty")
-
     if last_name:
-        return f"My name is {first_name} {last_name}"
-    return f"My name is {first_name}"
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}
