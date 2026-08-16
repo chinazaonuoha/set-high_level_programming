1e582b28fb5c99051d0d0f389bd074b2bfb677ba
chinazaonuoha <developerworkpace@gmail.com>
2026-08-16 17:20:30 +0100

    Write a function that prints a square with the character #.

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -12,12 +12,12 @@ def print_square(size):
 
     Raises:
         TypeError: If size is not an integer.
-        ValueError: If size is less than or equal to 0.
+        ValueError: If size is less than 0.
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-    if size <= 0:
-        raise ValueError("size must be greater than 0")
+    if size < 0:
+        raise ValueError("size must be >= 0")
 
     for _ in range(size):
         print('#' * size)
