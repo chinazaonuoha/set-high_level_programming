462fa56103a09d41af7031b3b468e7ad154f14f9
chinazaonuoha <developerworkpace@gmail.com>
2026-08-16 19:20:43 +0100

    write unittests for the function def max_integer(list=[]):.

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,6 +8,10 @@ from 6-max_integer import max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+
+        
     def test_max_integer(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([4, 3, 2, 1]), 4)
