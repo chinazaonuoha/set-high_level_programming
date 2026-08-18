111467caebb73b16cfe9ee4c9b6b0c68a3dcb68b
chinazaonuoha <developerworkpace@gmail.com>
2026-08-18 15:10:56 +0100

    Write the class Rectangle that inherits from Base:

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,49 +1,61 @@
-from models.base import Base
-
+#!/usr/bin/python3
 """
 This module defines the Rectangle class, which inherits from the Base class.
-The Rectangle class represents a rectangle shape with attributes for width, height,
-and its position (x, y) on a 2D plane. It provides methods to calculate the area,
-Display the rectangle using ASCII characters,
-and update its attributes. The class also includes
-validation for the attributes to ensure they are of the correct type and value.
+The Rectangle class represents a rectangle
+shape with attributes for width, height,
+and its position (x, y) on a 2D plane.
 """
+
+from models.base import Base
+
+
 class Rectangle(Base):
+    """Represent a Rectangle model inheriting from Base."""
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a Rectangle instance."""
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
+        """Width getter."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Width setter."""
         self.__width = value
 
     @property
     def height(self):
+        """Height getter."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Height setter."""
         self.__height = value
 
     @property
     def x(self):
+        """X coordinate getter."""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """X coordinate setter."""
         self.__x = value
 
     @property
     def y(self):
+        """Y coordinate getter."""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """Y coordinate setter."""
         self.__y = value
