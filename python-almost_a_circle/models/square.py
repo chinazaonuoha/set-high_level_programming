33f51c290e764fc858aacad849dec9200b430eb7
chinazaonuoha <developerworkpace@gmail.com>
2026-08-19 11:58:15 +0100

    Write the class Square that inherits from Rectangle:

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,8 +7,8 @@ class Square(Rectangle):
     """Represent a Square class that inherits from Rectangle."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a Square instance."""
-        super().__init__(id, size, size, x, y)
+        """Initialize a new Square."""
+        super().__init__(size, size, x, y, id)
        
     def __str__(self):
         """Return the print and str representation of the Square."""
